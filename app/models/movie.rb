5f9ddfa417fb3aa9ad66ff0272597d07fd16a79e
Ruslan <ruslan.n.kornienko@gmail.com>
2014-06-01 21:49:12 +0300

    validates_add

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,7 @@
 class Movie < ActiveRecord::Base
-  validates :title, presence: true
-  validates :rating, presence: true
+  validates :title, :rating, :release_date, presence: true
+  validates :title, :uniqueness => true
+  validates_format_of :rating, with: /\A(G|PG|PG-13|NC-17|R)\z/, :message => "must be G, PG, PG-13, NC-17 or R!"
+  validates_format_of :release_date, with: /\A\d{4}-\d{2}\d{2}./, :message => "format must be xxxx-xx-xx!"
+
 end
