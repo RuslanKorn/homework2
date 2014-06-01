f6e85e4cea97fb2c78ba1e7f5819b5783238c102
Ruslan <ruslan.n.kornienko@gmail.com>
2014-06-01 23:10:43 +0300

    Table with date instead of datetime and add validation

--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,7 @@ ActiveRecord::Schema.define(version: 20140528064816) do
     t.string   "title"
     t.string   "rating"
     t.text     "description"
-    t.datetime "release_date"
+    t.date     "release_date"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
