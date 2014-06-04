e7988baa8dae47f3395eedbbe5637479eadc7057
Ruslan <ruslan.n.kornienko@gmail.com>
2014-06-04 19:48:24 +0300

    Select for ratings

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,7 +1,6 @@
 class Movie < ActiveRecord::Base
   validates :title, :rating, presence: true
   validates :title, uniqueness: true
-  validates_format_of :rating, with: /\A(G|PG|PG-13|NC-17|R)\z/, :message => "must be G, PG, PG-13, NC-17 or R"
   validates_format_of :release_date, with: /\A(\d{4}-\d{2}-\d{2})\z/, :message => "format must be yyyy-mm-dd"
 
 
