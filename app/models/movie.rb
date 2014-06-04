2f9db31c662830b0db4608b15fa6051fd7eb2966
Ruslan <ruslan.n.kornienko@gmail.com>
2014-06-05 00:15:56 +0300

    Sort with arrows

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,5 @@
 class Movie < ActiveRecord::Base
-  validates :title, :rating, presence: true
+  validates :title, :rating, :description, presence: true
   validates :title, uniqueness: true
   validates_format_of :release_date, with: /\A(\d{4}-\d{2}-\d{2})\z/, :message => "format must be yyyy-mm-dd"
 
