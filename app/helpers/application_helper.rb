568af2376c525a1aa5b7d15b2f9b59b02fbf33f2
Ruslan <ruslan.n.kornienko@gmail.com>
2014-06-07 11:18:43 +0300

    Sortable in helper and add rspec

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+	def sortable(col, title)
+    title = title.titleize
+		link_to_if(session[:sort_by] != col && session[:sort_by] != "#{col} DESC", title,  movies_path(sort_by: col) ) do 
+      link_to_if(session[:sort_by] == "#{col} DESC", "#{title} &dArr;".html_safe,  movies_path(sort_by: col) ) do
+        link_to_if(session[:sort_by] == col, "#{title} &uArr;".html_safe,  movies_path(sort_by: "#{col} DESC") )
+      end
+    end 
+  end
 end
