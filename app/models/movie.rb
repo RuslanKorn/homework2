d6f3cd8bc4f503ac144e2f6772bc5ffbe1788103
Ruslan <ruslan.n.kornienko@gmail.com>
2014-06-09 16:12:59 +0300

    Add test scope :list

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,7 +3,14 @@ class Movie < ActiveRecord::Base
   validates :title, uniqueness: true
   validates_format_of :release_date, with: /\A(\d{4}-\d{2}-\d{2})\z/, :message => "format must be yyyy-mm-dd"
 
+scope :list, ->(options) {
+    res = all
+    res = res.where(rating: options[:rating]) if options.key? :rating
+    res = res.order(options[:order]) if options.key? :order
+    res
+  }
+
   def self.all_ratings
-    Movie.select(:rating).distinct.pluck(:rating)
+    all.map(&:rating).uniq
   end
 end
--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -27,6 +27,55 @@ describe Movie do
       Movie.create(title: 'bbb', rating: 'PG', release_date: '1992-02-05', description: 'Nice movie.')
       expect(Movie.all_ratings).to eq(['PG'])
     end
+  end
+
+  context ".list" do
+    let!(:movie_one) {Movie.create(title: 'bbb', rating: 'PG', release_date: '1992-02-05', description: 'Nice movie.')}
+    let!(:movie_two) {Movie.create(title: 'aaa', rating: 'G', release_date: '1994-02-05', description: 'Nice movie.')}
+    let!(:movie_three) {Movie.create(title: 'zzz', rating: 'NC-17', release_date: '1990-02-05', description: 'Nice movie.')}
+    it "Order by title ASC" do
+      Movie.list(:order => 'title').should eq([movie_two, movie_one, movie_three])
+    end
+
+    it "Order by title DESC" do
+      Movie.list(:order => 'title DESC').should eq([movie_three, movie_one, movie_two])
+    end
+
+    it "Order by release date ASC" do
+      Movie.list(:order => 'release_date').should eq([movie_three, movie_one, movie_two])
+    end
+
+    it "Order by release date DESC" do
+      Movie.list(:order => 'release_date DESC').should eq([movie_two, movie_one, movie_three])
+    end
+
+    it "One rating is select" do
+      Movie.list(:rating => ['G']).should eq([movie_two])
+    end
+
+    it "All ratings is selected" do
+      Movie.list(:rating => ['PG', 'G', 'NC-17']).should eq([movie_one, movie_two, movie_three])
+    end
+
+    it "Two ratings, order by title ASC" do
+      Movie.list(:rating => ['G', 'NC-17'], :order => 'title').should eq([movie_two, movie_three])
+    end
+
+    it "Two ratings, order by title DESC" do
+      Movie.list(:rating => ['G', 'NC-17'], :order => 'title DESC').should eq([movie_three, movie_two])
+    end
+    
+    it "Two ratings, order by release date ASC" do
+      Movie.list(:rating => ['G', 'NC-17'], :order => 'release_date').should eq([movie_three, movie_two])
+    end
+
+    it "Two ratings, order by release date DESC" do
+      Movie.list(:rating => ['G', 'NC-17'], :order => 'release_date DESC').should eq([movie_two, movie_three])
+    end
+
+    it "Empty rating" do
+      Movie.list(:rating => []).should eq([])
+    end
 
   end
 end
