f034bb16029eab4191e8d83d127dc1d18f71a2ac
Ruslan <ruslan.n.kornienko@gmail.com>
2014-06-17 12:24:56 +0300

    Add column image and default image

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,5 @@
 class Movie < ActiveRecord::Base
-  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => 'question_:style.jpg'
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
   validates :title, :rating, :description, presence: true
   validates :title, uniqueness: true
